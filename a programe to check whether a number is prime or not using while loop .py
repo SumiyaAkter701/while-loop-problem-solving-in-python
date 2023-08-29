0506594e03439f6dbdca649a928a465897527ec0
SumiyaAkter701 <143332814+SumiyaAkter701@users.noreply.github.com>
2023-08-29 23:43:29 +0600

    Update a programe to check whether a number is prime or not using while loop .py

--- a programe to check whether a number is prime or not using while loop .py
+++ a programe to check whether a number is prime or not using while loop .py
@@ -1,1 +1,12 @@
 # while-loop-problem-solving-in-python
+num = int (input("Type a number: "))
+divided_by = 2
+verifyprimenumber = 0
+while divided_by< num:
+    if num%divided_by==0:
+        verifyprimenumber+=1
+    divided_by+=1
+if verifyprimenumber ==0:
+    print("This is a Prime Number")
+else:
+    print("This is not a Prime Number")
